3869db5c4b2de4e7d407f88dc75160db28825952
frontend2wd <iqbalkiboo@gmail.com>
2021-10-28 10:20:57 +0700

    change title head

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -48,7 +48,7 @@ const App = () => {
   return (
     <Container>
       <TodoContainer onSubmit={handleSubmit}>
-        <Title>Redux Saga Todo List</Title>
+        <Title>Majoo Indonesia Todo List</Title>
 
         <InputContainer>
           <Input
